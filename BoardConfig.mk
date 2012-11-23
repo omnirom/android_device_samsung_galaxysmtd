4ab8a8ed196a35835ce27442d0bdbf27a07a5a67
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-23 14:27:27 +0700

    galaxysmtd: add bluetooth configuration
    Change-Id: Ic1315f721db905fe345195faf85e7410a1c0aa62

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,9 @@ BOARD_HAVE_FM_RADIO := true
 BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 BOARD_FM_DEVICE := si4709
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/galaxysmtd/bluetooth
+
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxysmtd/recovery/recovery_keys.c
 
