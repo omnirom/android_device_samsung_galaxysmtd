c30290142b98deb7e773674b9424f6e99e816bc6
SferaDev <sferarc@gmail.com>
2013-10-22 16:39:27 +0200

    galaxysmtd: Kit Kat bring-up
    This reverts commit ad3c7b2ba98712c3252cb7b7f40add485e0887be.
    This adds samsung stk boolean.
    This adds gsm.mk call
    This adds omni dependencies (kernel missing on omnirom, just fork)
    This comments some overlays
    
    Change-Id: If4ddd8b11a74dd032133ba56891fba2adef446b1

--- omni_galaxysmtd.mk
+++ omni_galaxysmtd.mk
@@ -1,13 +1,28 @@
-# Inherit device configuration
-$(call inherit-product, device/samsung/galaxysmtd/full_galaxysmtd.mk)
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Release name
-PRODUCT_RELEASE_NAME := GalaxyS
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-## Device identifier. This must come after all inclusions
+# Inherit device configuration
+$(call inherit-product, device/samsung/galaxysmtd/full_galaxysmtd.mk)
+
+# Device identifier
+PRODUCT_RELEASE_NAME := GalaxyS
 PRODUCT_DEVICE := galaxysmtd
 PRODUCT_NAME := omni_galaxysmtd
 PRODUCT_BRAND := samsung
