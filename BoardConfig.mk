240cd8a0ba751b1faf0fff4b79f41844584fcd1c
Humberto Borba <humberos@gmail.com>
2014-01-08 23:31:48 -0200

    OmniROM: update environment
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ie5ecfb7616965c795b7afdbdd5c56a5e76d94b14

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@
 BOARD_SECOND_CAMERA_DEVICE := /dev/video2
 
 # Kernel Config
-TARGET_KERNEL_CONFIG := cyanogenmod_galaxysmtd_defconfig
+TARGET_KERNEL_CONFIG := custom_galaxysmtd_defconfig
 
 # FM Radio
 BOARD_HAVE_FM_RADIO := true
