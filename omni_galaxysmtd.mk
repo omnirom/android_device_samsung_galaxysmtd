2f5ffe2b1139ddf12e12f2a119f4788ce4b5ace0
Markus Guidry <markus@gu1dry.com>
2014-03-10 17:59:37 -0400

    galaxysmtd: Device makefile cleanup
    -Standardizes the layout of omni_galaxysmtd.mk
    -Restores CM's copyright
    -Remove PRODUCT_RELEASE_NAME, this isn't used in Omnirom
    
    Change-Id: Id612c261a0ffc7bf053acba0a6e0b05f7a17aee8

--- omni_galaxysmtd.mk
+++ omni_galaxysmtd.mk
@@ -1,4 +1,6 @@
+#
 # Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#
 
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
@@ -21,18 +21,25 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit device configuration
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit more device specific configurations
 $(call inherit-product, device/samsung/galaxysmtd/device.mk)
 
 # Galaxy S uses high-density artwork where available
 PRODUCT_LOCALES += hdpi
 
-# Device identifier
-PRODUCT_RELEASE_NAME := GalaxyS
-PRODUCT_DEVICE := galaxysmtd
-PRODUCT_NAME := omni_galaxysmtd
-PRODUCT_BRAND := samsung
+# Discard inherited values and use our own instead.
 PRODUCT_MODEL := GT-I9000
+PRODUCT_BRAND := samsung
+PRODUCT_NAME := omni_galaxysmtd
+PRODUCT_DEVICE := galaxysmtd
+PRODUCT_MANUFACTURER := samsung
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9000 TARGET_DEVICE=GT-I9000 BUILD_FINGERPRINT=samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=GT-I9000 \
+    TARGET_DEVICE=GT-I9000 \
+    PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys" \
+    BUILD_FINGERPRINT="samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys"
